a16f691096bc87b6a9762bef138b2afefb6c9cd3
einbulinda <einbulinda@gmail.com>
2021-08-03 18:52:59 +0300

    Creates ProductCard,fetching data with useEffect, error handling

---
+++ src/components/ProductCard.js
@@ -0,0 +1,25 @@
+import { Button, Card } from "react-bootstrap";
+
+const ProductCard = ({ products }) => {
+  return (
+    <div className="d-flex p-2 m-2">
+      {products.map((product) => (
+        <Card className="m-3" style={{ width: "22rem" }} key={product.id}>
+          <Card.Img variant="top" src={product.image} />
+          <Card.Body className="mb-0">
+            <Card.Title>{product.name}</Card.Title>
+            <Card.Text>{product.category}</Card.Text>
+            <Card.Text>Ksh. {product.price}</Card.Text>
+            <Card.Text>{product.rating} Rating</Card.Text>
+            <Card.Text>{product.reviews} Reviews</Card.Text>
+            <Button variant="primary"> (icon)Add To Cart</Button>
+            {/* Add to wishlist heart on top right side */}
+            {/* Conditional sale banner on left side...bookmarker shape */}
+          </Card.Body>
+        </Card>
+      ))}
+    </div>
+  );
+};
+
+export default ProductCard;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,36 @@
+import ProductCard from "../components/ProductCard";
+import { useEffect } from "react";
+import { useState } from "react";
+
 const Home = () => {
+  const [products, setProducts] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/products")
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("Could not fetch data for the resource from server");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setProducts(data);
+        setIsLoading(false);
+        setError(null);
+      })
+      .catch((err) => {
+        setIsLoading(false);
+        setError(err.message);
+      });
+  }, []);
+
   return (
     <main>
-      <h1>This is my Home Page</h1>
+      {error && <div>{error}</div>}
+      {isLoading && <div>Loading...</div>}
+      {products && <ProductCard products={products} />}
     </main>
   );
 };
