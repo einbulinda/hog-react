f7c0ad856dc02eb762f1af11a9619b57763a8d9b
einbulinda <einbulinda@gmail.com>
2021-08-11 19:13:54 +0300

    Refactors Register to use useState Hook

--- src/pages/UserPages/Register.js
+++ src/pages/UserPages/Register.js
@@ -1,42 +1,30 @@
-import { Component } from "react";
+import { useState } from "react";
 import { Button, Card, Container, Form, ListGroup } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import FormInput from "../../components/Forms/FormInput";
 import { auth, handleUserProfile } from "../../firebase/utils";
 
-const initialState = {
-  displayName: "",
-  email: "",
-  password: "",
-  confirmPassword: "",
-  errors: [],
-};
+const Register = (props) => {
+  const [displayName, setDisplayName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [errors, setErrors] = useState([]);
 
-class Register extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      ...initialState,
-    };
-    this.handleChange = this.handleChange.bind(this);
-  }
+  const resetForm = () => {
+    setDisplayName("");
+    setEmail("");
+    setPassword("");
+    setConfirmPassword("");
+    setErrors([]);
+  };
 
-  handleChange(e) {
-    const { name, value } = e.target;
-    this.setState({
-      [name]: value,
-    });
-  }
-
-  handleFormSubmit = async (e) => {
+  const handleFormSubmit = async (e) => {
     e.preventDefault();
-    const { displayName, email, password, confirmPassword } = this.state;
 
     if (password !== confirmPassword) {
       const err = ["Passwords provided do not match."];
-      this.setState({
-        errors: err,
-      });
+      setErrors([err]);
       return;
     }
 
@@ -46,87 +34,82 @@ class Register extends Component {
         password
       );
       await handleUserProfile(user, { displayName });
-      this.setState({ ...initialState });
+      resetForm();
     } catch (error) {
       console.log(error);
     }
   };
 
-  render() {
-    const { displayName, email, password, confirmPassword, errors } =
-      this.state;
-
-    return (
-      <Container className="py-4">
-        <Card className="p-2 text-center mx-auto" style={{ width: "50rem" }}>
-          <Card.Header as="h2">REGISTER ACCOUNT</Card.Header>
-          <Card.Body>
-            {errors.length > 0 && (
-              <ListGroup className="mb-3" variant="flush">
-                {errors.map((err, index) => {
-                  return (
-                    <ListGroup.Item
-                      variant="danger"
-                      className="text-danger"
-                      key={index}
-                    >
-                      {err}
-                    </ListGroup.Item>
-                  );
-                })}
-              </ListGroup>
-            )}
-            <Form onSubmit={this.handleFormSubmit}>
-              <FormInput
-                type="text"
-                name="displayName"
-                value={displayName}
-                placeholder="Enter Name"
-                onChange={this.handleChange}
-                inputIcon="user"
-                size="lg"
-              />
-              <FormInput
-                type="email"
-                name="email"
-                value={email}
-                placeholder="Email address"
-                onChange={this.handleChange}
-                inputIcon="envelope"
-                size="lg"
-              />
-              <FormInput
-                type="password"
-                name="password"
-                value={password}
-                placeholder="Enter Password"
-                onChange={this.handleChange}
-                inputIcon="lock"
-                size="lg"
-              />
-              <FormInput
-                type="password"
-                name="confirmPassword"
-                value={confirmPassword}
-                placeholder="Confirm Password"
-                onChange={this.handleChange}
-                inputIcon="lock"
-                size="lg"
-              />
-              <Button type="submit" variant="dark" className="px-4" size="lg">
-                REGISTER
-              </Button>
-            </Form>
-            <Card.Text className="my-3">
-              <span>
-                <Link to="/account">Sign In </Link>
-              </span>
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      </Container>
-    );
-  }
-}
+  return (
+    <Container className="py-4">
+      <Card className="p-2 text-center mx-auto" style={{ width: "50rem" }}>
+        <Card.Header as="h2">REGISTER ACCOUNT</Card.Header>
+        <Card.Body>
+          {errors.length > 0 && (
+            <ListGroup className="mb-3" variant="flush">
+              {errors.map((err, index) => {
+                return (
+                  <ListGroup.Item
+                    variant="danger"
+                    className="text-danger"
+                    key={index}
+                  >
+                    {err}
+                  </ListGroup.Item>
+                );
+              })}
+            </ListGroup>
+          )}
+          <Form onSubmit={handleFormSubmit}>
+            <FormInput
+              type="text"
+              name="displayName"
+              value={displayName}
+              placeholder="Enter Name"
+              onChange={(e) => setDisplayName(e.target.value)}
+              inputIcon="user"
+              size="lg"
+            />
+            <FormInput
+              type="email"
+              name="email"
+              value={email}
+              placeholder="Email address"
+              onChange={(e) => setEmail(e.target.value)}
+              inputIcon="envelope"
+              size="lg"
+            />
+            <FormInput
+              type="password"
+              name="password"
+              value={password}
+              placeholder="Enter Password"
+              onChange={(e) => setPassword(e.target.value)}
+              inputIcon="lock"
+              size="lg"
+            />
+            <FormInput
+              type="password"
+              name="confirmPassword"
+              value={confirmPassword}
+              placeholder="Confirm Password"
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              inputIcon="lock"
+              size="lg"
+            />
+            <Button type="submit" variant="dark" className="px-4" size="lg">
+              REGISTER
+            </Button>
+          </Form>
+          <Card.Text className="my-3">
+            <span>
+              <Link to="/account">Sign In </Link>
+            </span>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </Container>
+  );
+};
 
 export default Register;
