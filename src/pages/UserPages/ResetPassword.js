c957c90bf01754338ca965593fbe89ef942ca058
einbulinda <einbulinda@gmail.com>
2021-08-11 19:22:31 +0300

    Refactors ResetPassword with useState Hooks

--- src/pages/UserPages/ResetPassword.js
+++ src/pages/UserPages/ResetPassword.js
@@ -1,36 +1,17 @@
-import { Component } from "react";
+import { useState } from "react";
 import { Button, Card, Container, Form, ListGroup } from "react-bootstrap";
 import { Link, withRouter } from "react-router-dom";
 import FormInput from "../../components/Forms/FormInput";
 import { auth } from "../../firebase/utils";
 
-const initialState = {
-  email: "",
-  errors: [],
-};
+const ResetPassword = (props) => {
+  const [email, setEmail] = useState("");
+  const [errors, setErrors] = useState([]);
 
-class ResetPassword extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      ...initialState,
-    };
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(e) {
-    const { name, value } = e.target;
-    this.setState({
-      [name]: value,
-    });
-  }
-
-  handleForgotPassword = async (e) => {
+  const handleForgotPassword = async (e) => {
     e.preventDefault();
 
     try {
-      const { email } = this.state;
-
       const config = {
         url: "http://localhost:3000/account",
       };
@@ -43,61 +24,56 @@ class ResetPassword extends Component {
         })
         .catch(() => {
           const err = ["Email not registered. Lets register!"];
-          this.setState({
-            errors: err,
-          });
+          setErrors([err]);
         });
     } catch (error) {
       console.log(error.message);
     }
   };
 
-  render() {
-    const { email, errors } = this.state;
-    return (
-      <Container className="p-4">
-        <Card className="col-sm-6 col-md-4 text-center mt-4 mx-auto">
-          <Card.Header as="h2">LOGIN</Card.Header>
-          <Card.Body>
-            <Form onSubmit={this.handleForgotPassword}>
-              {errors.length > 0 && (
-                <ListGroup className="mb-3" variant="flush">
-                  {errors.map((err, index) => {
-                    return (
-                      <ListGroup.Item
-                        variant="danger"
-                        className="text-danger"
-                        key={index}
-                      >
-                        <Link className="text-danger" to="/register">
-                          {err}
-                        </Link>
-                      </ListGroup.Item>
-                    );
-                  })}
-                </ListGroup>
-              )}
-              <FormInput
-                type="email"
-                name="email"
-                value={email}
-                placeholder="Enter registered email"
-                inputIcon="user"
-                size="lg"
-                onChange={this.handleChange}
-              />
-              <Button type="submit" variant="dark" className="px-4" size="lg">
-                RESET PASSWORD
-              </Button>
-            </Form>
-            <Card.Text className="p-3">
-              <Link to="/account">Sign In</Link>
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      </Container>
-    );
-  }
-}
+  return (
+    <Container className="p-4">
+      <Card className="col-sm-6 col-md-4 text-center mt-4 mx-auto">
+        <Card.Header as="h2">LOGIN</Card.Header>
+        <Card.Body>
+          <Form onSubmit={handleForgotPassword}>
+            {errors.length > 0 && (
+              <ListGroup className="mb-3" variant="flush">
+                {errors.map((err, index) => {
+                  return (
+                    <ListGroup.Item
+                      variant="danger"
+                      className="text-danger"
+                      key={index}
+                    >
+                      <Link className="text-danger" to="/register">
+                        {err}
+                      </Link>
+                    </ListGroup.Item>
+                  );
+                })}
+              </ListGroup>
+            )}
+            <FormInput
+              type="email"
+              name="email"
+              value={email}
+              placeholder="Enter registered email"
+              inputIcon="user"
+              size="lg"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Button type="submit" variant="dark" className="px-4" size="lg">
+              RESET PASSWORD
+            </Button>
+          </Form>
+          <Card.Text className="p-3">
+            <Link to="/account">Sign In</Link>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </Container>
+  );
+};
 
 export default withRouter(ResetPassword);
